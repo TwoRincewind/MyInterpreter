990e0ebdae4b0dff78cf6bd29dc6201ffd3bebda
Theodor <fedor@poksh.ru>
2025-10-15 19:05:17 +0300

    some comments and a bit different errors output

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import re
+import os
 from string import whitespace
-import traceback
 
-from Lists.List_absolute_lambda import *
-from Tokens import *
+from Lists.List_absolute_lambda import car, cdr, cons, isList
+from Tokens import ourEn, SF, BO, BP, Symbol
 
 
 NIL = cons(None, None)
-EVAL_NIL = lambda: NIL # TODO
 
 
 def show(v: tuple) -> str:
@@ -41,7 +40,7 @@ IGNORED = whitespace + ','
 
 def processString(s: str) -> str:
     s = s.lstrip(IGNORED)
-    while s and s[0] == ';': # comments handling
+    while s and s[0] == ';':  # comments handling
         line_end: int = s.find('\n')
         if line_end < 0:
             return ''
@@ -58,38 +57,36 @@ def findpref(s: str, breakers: str = IGNORED + MEANINGFUL) -> str:
 
 
 def parse_token(token: str) -> str | ourEn:
-    for En in [SF, BO, BP]:
-        if token in En:
-            return En(token)
+    for en in [SF, BO, BP]:
+        if token in en:
+            return en(token)
     if token == 'true':
         return True
     if token == 'false':
         return False
     try:
         return int(token)
-    except:
+    except ValueError:
         if token == 'inf':
             return Symbol(token)
         try:
             return float(token)
-        except:
+        except ValueError:
             return Symbol(token)
 
 
-def prs(s: str) -> tuple: # (ast, remainder of s), ast = None if nothing was parsed
+def prs(s: str) -> tuple:  # (ast, remainder of s), ast = None if nothing was parsed
     s = processString(s)
     if not s:
         return (None, s)
-
     match s[0]:
-        case '"': # string
+        case '"':  # string
             matching = s.find('"', 1)
             if matching < 0:
-                raise SyntaxError (f'the closing " is missing: {s}')
+                raise SyntaxError(f'the closing " is missing: {s}')
             return (s[1:matching], s[matching + 1:])
-
-        case '(': # list
-            cache = s # saving initial view for Error illustration
+        case '(':  # list
+            cache = s  # saving initial view for Error illustration
             s = s[1:]
             arr: list = []
             while s and s[0] != ')':
@@ -97,42 +94,43 @@ def prs(s: str) -> tuple: # (ast, remainder of s), ast = None if nothing was par
                 arr.append(elem)
                 s = processString(s)
             if not s:
-                raise SyntaxError (f'the closing ) is missing: {cache}')
+                raise SyntaxError(f'the closing ) is missing: {cache}')
             result = NIL
             for elem in reversed(arr):
                 result = cons(elem, result)
             return (result, s[1:])
-
         case ')':
-            raise SyntaxError (f'extra closing bracket: {s}') # TODO debug
-
+            raise SyntaxError(f'extra closing bracket: {s}')  # TODO debug
         case "'":
-            # try:
-            v, rem = prs(s[1:])
-            # except Exception as e:
-            #     raise RuntimeError (f'invalid quote: {s}') from e
+            try:
+                v, rem = prs(s[1:])
+            except Exception as e:
+                raise RuntimeError(f'invalid quote: {s}') from e
             if v is None:
-                raise SyntaxError (f'quote of nothing: {s}')
+                raise SyntaxError(f'quote of nothing: {s}')
             return (cons(SF.QUOTE, cons(v, NIL)), rem)
-
-        case _: # maybe we can find token
-            first = findpref(s) # TODO recognize token
+        case _:  # maybe we can find token
+            first = findpref(s)
             return (parse_token(first) if first else None, s[len(first):])
 
 
-def get_elems(v, n):
+EVAL_NIL = lambda: NIL
+repr = lambda v: v if type(v) is str else show(v)
+
+
+def get_elems(v, n):  # get all elements from list with length n
     ret = []
     while v != NIL and len(ret) < n:
         ret.append(car(v))
         v = cdr(v)
     if v != NIL:
-        raise SyntaxError ('extra elems')
+        raise SyntaxError('extra elems')
     if len(ret) < n:
-        raise SyntaxError ('lack of elems')
+        raise SyntaxError('lack of elems')
     return ret
 
 
-def binop(op, a, b):
+def binop(op, a, b):  # apply binary operation to 2 args
     match op:
         case BO.ADD:
             return a + b
@@ -148,7 +146,7 @@ def binop(op, a, b):
             return a % b
 
 
-def binpred(op, a, b):
+def binpred(op, a, b):  # apply binary predicat
     match op:
         case BP.LT:
             return a < b
@@ -158,14 +156,10 @@ def binpred(op, a, b):
             return a == b
 
 
-def repr2str(v):
-    return v if type(v) is str else show(v)
-
-
 def eval_naive(v):
-    if isList(v):
+    if isList(v):  # call car with cdr as args
         if v == NIL:
-            EVAL_NIL()
+            return EVAL_NIL()
         h = eval_naive(car(v))
         t = cdr(v)
         if isinstance(h, BO):
@@ -176,22 +170,22 @@ def eval_naive(v):
                 t = cdr(t)
                 b = car(t)
                 if t == NIL:
-                    return repr2str(eval_naive(a))
+                    return repr(eval_naive(a))
                 t = cdr(t)
                 if t != NIL:
                     raise SyntaxError
-                return repr2str(eval_naive(a)) + repr2str(eval_naive(b))
+                return repr(eval_naive(a)) + repr(eval_naive(b))
             try:
                 a, b = get_elems(t, 2)
                 return binop(h, eval_naive(a), eval_naive(b))
             except SyntaxError as e:
-                raise SyntaxError (f'wrong args for BO: {show(v)}') from e
+                raise SyntaxError(f'wrong args for BO: {show(v)}') from e
         elif isinstance(h, BP):
             try:
                 a, b = get_elems(t, 2)
                 return binpred(h, eval_naive(a), eval_naive(b))
             except SyntaxError as e:
-                raise SyntaxError (f'wrong args for BP: {show(v)}') from e
+                raise SyntaxError(f'wrong args for BP: {show(v)}') from e
         elif isinstance(h, SF):
             match h:
                 case SF.QUOTE:
@@ -206,74 +200,67 @@ def eval_naive(v):
                 case SF.CAR:
                     a = eval_naive(get_elems(t, 1)[0])
                     if not isList(a):
-                        return a # config moment
+                        return a  # config moment
                     if a == NIL:
-                        raise SyntaxError ("NIL's head???") # config moment
+                        raise SyntaxError("NIL's head???")  # config moment
                     return car(a)
                 case SF.CDR:
                     a = eval_naive(get_elems(t, 1)[0])
                     if not isList(a):
-                        return NIL # config moment
+                        return NIL  # config moment
                     if a == NIL:
-                        raise SyntaxError ("NIL's elems???") # config moment
+                        raise SyntaxError("NIL's elems???")  # config moment
                     return cdr(a)
                 case _:
                     raise SyntaxError
         else:
-            raise SyntaxError ('wrong head form')
+            raise SyntaxError('wrong head form')
     return v
 
 
 def repl():
-    pref = '>>> '
-    def exit():
-        print('exiting REPL...')
-
-    exiters, loaders = map(lambda s: s.split(), [':q :quit :exit', ':l :load'])
-
+    pref, suff = '>>> ', 'exiting REPL...'
+    strs = [':q :quit :exit exit quit', ':l :load']
+    exiters, loaders = map(lambda s: s.split(), strs)
     while True:
         try:
             inp = input(pref).strip()
         except KeyboardInterrupt:
-            exit()
+            print(suff)
             break
-        except EOFError: # when started with file input
+        except EOFError:  # when started with file input
             print('input exhausted, exiting REPL...')
             break
-
         if not inp:
             continue
         first = findpref(inp, whitespace)
-
         if first in exiters:
-            exit()
+            print(suff)
             break
-
         if first in loaders:
-            filename = s[len(first)].strip()
+            filename = inp[len(first)].strip()
             if not filename:
                 print(pref + 'Error: no filename provided.')
                 continue
             if not os.path.isfile(filename):
                 print(pref + f'Error: no such file "{filename}"')
                 continue
-            with open(filename, 'r', encoding='utf-8') as f: # TODO execution
+            with open(filename, 'r', encoding='utf-8') as f:  # TODO execution
                 print(pref + f'Content of {filename}:')
                 print(f.read())
                 print(pref + f'End of {filename}.')
             continue
-
         try:
-            while inp: # parse multiple objects on single input line
+            while inp:  # parse multiple objects on single input line
                 res, inp = prs(inp)
                 to_print = show(eval_naive(res))
                 if to_print:
                     print(to_print)
         except Exception as e:
-            print(f'{e.__class__.__name__}')
-            print('\t', e)
-            # traceback.print_exc(e)
+            while e:  # and e not in seen:
+                print(e)
+                # print(f"{type(e).__name__}: {e}")
+                e = e.__cause__  # if e.__cause__ is not None else e.__context__
 
 
 repl()
-
