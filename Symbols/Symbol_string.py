1f5df161baa6050d789f9177ff0506200a79b5a0
Theodor <fedor@poksh.ru>
2025-10-22 20:14:04 +0300

    changed symbols usage in environment

--- Symbols/Symbol_string.py
+++ Symbols/Symbol_string.py
@@ -6,5 +6,9 @@ def symbol(name: str) -> str:
 def symname(sym: str) -> str:
     return sym[1:]
 
+def symkey(sym: str) -> str:
+    return sym
+
+
 def isSymbol(obj) -> bool:
     return isinstance(obj, str) and obj[0] == pref
